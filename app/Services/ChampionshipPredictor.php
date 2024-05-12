39b79c14cc153a236d2dc54a713a37fb695964b5
Yusuf Özdemir <yusuf@ozdemir.be>
2024-05-12 15:38:27 +0300

    remove week restrictions && on the last matches, consider gd

--- app/Services/ChampionshipPredictor.php
+++ app/Services/ChampionshipPredictor.php
@@ -10,9 +10,6 @@ class ChampionshipPredictor
     {
 
         // if the championship is over or there are more than 3 weeks left, return empty
-        if ($weeks_left == 0 || $weeks_left >= 3) {
-            return collect();
-        }
 
         // max point, it can be the championship point. we don't know that yet.
         $max = $pts->max();
@@ -23,6 +20,7 @@ class ChampionshipPredictor
         // total result count
         $totalResult = $predictions->flatten()->count();
 
+
         // calculate the percentage of each item
         return $predictions->map(function($item) use ($totalResult) {
             return round(collect($item)->flatten()->count() / $totalResult * 100, 2);
@@ -32,28 +30,30 @@ class ChampionshipPredictor
     public static function predictNextRoundsPoints($points, $max, $iterate): Collection
     {
         $predictions = [];
+        $isFirst = true;
         foreach ($points as $key => $sayi) {
             $predictions[$key][$sayi] = [];
 
-            array_push($predictions[$key][$sayi], ...self::addPoints($sayi, $max, $iterate));
+            array_push($predictions[$key][$sayi], ...self::addPoints($sayi, $max, $iterate, $isFirst));
+            $isFirst = false;
         }
         return collect($predictions);
     }
 
-    public static function addPoints($point, $max, $iterate): Collection
+    public static function addPoints($point, $max, $iterate, $isFirst): Collection
     {
         $results = collect();
         // recursive call
         if ($iterate > 1) {
             $iterate--;
-            $results->push(...self::addPoints($point + 3, $max, $iterate));
-            $results->push(...self::addPoints($point + 1, $max, $iterate));
+            $results->push(...self::addPoints($point + 3, $max, $iterate, $isFirst));
+            $results->push(...self::addPoints($point + 1, $max, $iterate, $isFirst));
         }
 
         return $results
             ->when($point + 3 >= $max)->push($point + 3)
             ->when($point + 1 >= $max)->push($point + 1)
-            ->when($point >= $max)->push($point);
+            ->when($iterate == 1 && $isFirst && $point >= $max)->push($point);
     }
 
 }
